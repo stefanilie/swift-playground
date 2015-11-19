0a6f3a62455ff36d774529ce2828e63688791fe8
Ilie <stefan.ilie@rootpm4045029a.net.work>
2015-11-19 16:40:25 +0200

    first commit

---
+++ Exercise - Variable Operations.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var first = 15
+var second = 5
+var sum = first + second
+var multiply = first * second
+var minus = first - second
+var division = first / second
+
+var one: Int, two: Int, three: Int
+let cst1: String, cst2: String, cst3: String
+
+var special: Double
+var specialInt: Int = 423
+var hello: String = "hello"
+var world: String = "World"
+
+var message = hello + world
+message = "\(hello) \(world)"
---
+++ Variables-types.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var strHelloWorld = "Hello, ciorba" //String
+strHelloWorld + " eu sunt miezu";
+
+var numA = 1.5 //Double
+var numB: Float = 1.5 //Float
+var numC = 3 //Int
+
+var sum = 5+1
+var product = 5*5
+var divisor = 20/5
+var remainder = 22%5
+var sub = 44-22
+
+var crazyResult = sum * product + (divisor * remainder + sub)
+
+var newStr = "Hey " + "how are you"
+var firstName = "Jon"
+var lastName = "Smith"
+
+var fullName = "\(firstName) \(lastName)"
+var price = "Price: $\(numA)"
+
+price = "Banana"
+
+let total = 5.1 //Constant
+
+var totalPrice: Double
+totalPrice = 1.56
+print(totalPrice)
