159db1f45390572feb3fe346152f5c2844968923
Stefan Ilie <ilie.stefan92@yahoo.com>
2016-10-13 12:42:11 +0300

    Finished Optionals Course

--- Swift3 and iOS10/Playgrounds/Optionals.playground/Contents.swift
+++ Swift3 and iOS10/Playgrounds/Optionals.playground/Contents.swift
@@ -1,5 +1,9 @@
 //: Playground - noun: a place where people can play
 
+
+// tl;dr:
+// ? - used for variables that we don't know if they will have a value
+// ! - used for variables that we know FOR SURE that at one point in time we add value
 import UIKit
 
 //Question mark is for when we don't know 
@@ -10,10 +14,10 @@ if lotteryWinnings != nil {
     print(lotteryWinnings!)
 }
 
-lotteryWinnings=10;
+lotteryWinnings = 100;
 
-if let winnings = lotteryWinnings {
-    print(winnings)
+if let winnings = lotteryWinnings{
+    print(winnings);
 }
 
 class Car {
@@ -22,12 +26,70 @@ class Car {
 
 var vehicle: Car?
 
-if let v = vehicle{
-    if let m = v.model{
-        print(m)
+vehicle = Car();
+//This is an optional and I don't know what is going to happen with it
+vehicle?.model = "Bronco";
+
+//If vehicle has a value, it adds it to v.
+if let v = vehicle {
+    if let m = v.model {
+        print(m);
     }
 }
 
+//This is the same as the above, but in one line
 if let v = vehicle, let m = v.model{
     print(m);
 }
+
+var cars: [Car]?
+
+cars = [Car]();
+
+if let carArr = cars{
+    if carArr.count>0{
+        //do stuff
+    }
+}
+
+if let carArr = cars , carArr.count > 0{
+    //do stuff
+    //same as abouve
+} else {
+    cars?.append(Car());
+    print(cars?.count);
+}
+
+class Person {
+    //This means that I am going to give this one a value sometime.
+    private var _age: Int!;
+    
+    var age: Int {
+        if _age == nil{
+            _age=15;
+        }
+        
+        return _age;
+    }
+    
+    func setAge(newAge: Int){
+        self._age = newAge;
+    }
+}
+
+var jack = Person();
+print(jack.age);
+
+class Doc {
+    var species: String;
+    
+    init(someSpecies: String){
+        self.species = someSpecies
+    }
+}
+var lab = Doc(someSpecies: "Black Lab");
+print(lab.species);
+
+
+
+
