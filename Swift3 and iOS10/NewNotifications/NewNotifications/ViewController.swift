45e8abdbb04175223be17406f3f7036c6fbdf507
Stefan Ilie <ilie.stefan92@yahoo.com>
2017-01-05 17:01:29 +0200

    started working on notifications app

---
+++ Swift3 and iOS10/NewNotifications/NewNotifications/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  NewNotifications
+//
+//  Created by Ilie, Stefan on 1/5/17.
+//  Copyright © 2017 Ilie, Stefan. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //request permissions
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {(granted, error) in
+            if granted {
+                print("Notification access granted");
+            } else {
+                print(error?.localizedDescription);
+            }
+        })
+    }
+    
+    @IBAction func notifyButtonTapped(sender: UIButton){
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification");
+            }
+        })
+    }
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()){
+        
+        let notif = UNMutableNotificationContent();
+        
+        notif.title = "New notification"
+        notif.subtitle = "These are great!"
+        notif.body = "The new notification options in iOS 10 are what I've always dreamed of!"
+        
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false);
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notificationTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+            if error != nil {
+                print(error)
+                completion(false);
+            } else {
+                completion(true);
+            }
+        
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
